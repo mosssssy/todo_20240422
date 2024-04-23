41bc927552447eebc79beea64774cb02a4de7c6f
gonmura <user@Gonmuras-MacBook-Pro.local>
2024-04-23 18:02:19 +0900

    imagepickerの導入

--- lib/views/my_page/edit_profile/edit_profile_page.dart
+++ lib/views/my_page/edit_profile/edit_profile_page.dart
@@ -1,11 +1,13 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:todo_20240422/common_widget/margin_sizedbox.dart';
 import 'package:todo_20240422/functions/global_functions.dart';
 import 'package:todo_20240422/views/my_page/components/blue_button.dart';
 
-class EditProfilePage extends StatelessWidget {
+class EditProfilePage extends StatefulWidget {
   const EditProfilePage({
     super.key,
     required this.userName,
@@ -13,12 +15,17 @@ class EditProfilePage extends StatelessWidget {
   final String userName;
 
   @override
+  State<EditProfilePage> createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController userNameController = TextEditingController();
+  final User user = FirebaseAuth.instance.currentUser!;
+
+  @override
   Widget build(BuildContext context) {
-    print(userName);
-    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-    final TextEditingController userNameController = TextEditingController();
-    final User user = FirebaseAuth.instance.currentUser!;
-    userNameController.text = userNameController.text;
+    userNameController.text = widget.userName;
     return Scaffold(
         appBar: AppBar(
           title: const Text(
@@ -36,6 +43,15 @@ class EditProfilePage extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    MarginSizedBox.mediumHeightMargin,
+                    BlueButton(
+                      buttonText: '画像を選択する',
+                      onBlueButtonPressed: () {
+                        //Image Pickerをインスタンス化
+                        getImageFromGallery();
+                      },
+                    ),
+                    MarginSizedBox.mediumHeightMargin,
                     TextFormField(
                         controller: userNameController,
                         maxLength: 12,
@@ -69,4 +85,29 @@ class EditProfilePage extends StatelessWidget {
                   ],
                 ))));
   }
+
+  Future getImageFromGallery() async {
+    File? image; //画像を入れる変数
+    final picker = ImagePicker();
+    final pickedFile =
+        await picker.pickImage(source: ImageSource.gallery); //アルバムから画像を取得
+
+    if (pickedFile != null) {
+      image = File(pickedFile.path);
+      print(image);
+    }
+    setState(() {});
+  }
+
+  // Future getImageFromCamera() async {
+  //   final pickedFile =
+  //       await picker.pickImage(source: ImageSource.camera); //カメラから画像を取得
+  //   setState(() {
+  //     //画面を再読込
+  //     if (pickedFile != null) {
+  //       //画像を取得できたときのみ実行
+  //       image = File(pickedFile.path); //取得した画像を代入
+  //     }
+  //   });
+  // }
 }
