7c7ec5862c292703bdcb986054cef9e5600e4241
gonmura <user@Gonmuras-MacBook-Pro.local>
2024-04-23 12:23:04 +0900

    会員登録・ログイン処理を追加

--- lib/views/auth/auth_page.dart
+++ lib/views/auth/auth_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_20240422/common_widget/margin_sizedbox.dart';
 import 'package:todo_20240422/views/auth/components/auth_text_form_field.dart';
@@ -7,34 +8,62 @@ class AuthPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
-    final TextEditingController emailcontroller = TextEditingController();
-    final TextEditingController passcontroller = TextEditingController();
+    final GlobalKey<FormState> formkey = GlobalKey<FormState>();
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passController = TextEditingController();
 
     return Scaffold(
       body: Form(
-        key: _formkey,
+        key: formkey,
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               AuthTextFormField(
-                controller: emailcontroller,
+                controller: emailController,
                 label: 'メールアドレス',
               ),
               MarginSizedBox.smallHeightMargin,
               AuthTextFormField(
-                controller: passcontroller,
+                controller: passController,
                 label: 'パスワード',
+              ),
+              const SizedBox(
+                width: double.infinity,
+                child: Text(
+                  'パスワードを忘れた方はこちら >',
+                  style: TextStyle(color: Colors.blue),
+                  textAlign: TextAlign.end,
+                ),
               ),
               MarginSizedBox.bigHeightMargin,
               ElevatedButton(
-                  onPressed: () {
-                    if (_formkey.currentState!.validate()) {
-                      // 成功
-                    } else {
-                      // 失敗
+                  onPressed: () async {
+                    if (formkey.currentState!.validate()) {
+                      // 失敗時に処理ストップ
+                      return;
+                    }
+                    // 成功
+                    try {
+                      final User? user = (await FirebaseAuth.instance
+                              .createUserWithEmailAndPassword(
+                                  email: emailController.text,
+                                  password: passController.text))
+                          .user;
+                      if (user != null) {
+                        print('ユーザーを登録しました');
+                      }
+                    } on FirebaseAuthException catch (error) {
+                      print(error.code);
+                      if (error.code == 'email-already-in-use') {
+                        print('すでに使われているメールアドレスです');
+                      }
+                      if (error.code == 'weak-password') {
+                        print('パスワードが弱すぎます');
+                      }
+                    } catch (error) {
+                      print('予期せぬエラーです');
                     }
                   },
                   style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
@@ -44,7 +73,27 @@ class AuthPage extends StatelessWidget {
                   )),
               MarginSizedBox.smallHeightMargin,
               ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    if (!formkey.currentState!.validate()) {
+                      return;
+                    }
+                    try {
+                      // メール/パスワードでログイン
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final User? user = (await auth.signInWithEmailAndPassword(
+                        email: emailController.text,
+                        password: passController.text,
+                      ))
+                          .user;
+                      if (user != null) {
+                        print('ログイン成功');
+                      } else {
+                        print('ログイン失敗');
+                      }
+                    } catch (e) {
+                      //
+                    }
+                  },
                   child: const Text(
                     'ログイン',
                     style: TextStyle(color: Colors.blue),
